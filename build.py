593f0d83739b7794d28c24619645acfbb61b2831
Adam Becker <ajbecker@gmail.com>
2013-03-12 22:04:57 -0700

    Added favicon and a webfont plus a little more style

--- build.py
+++ build.py
@@ -3,18 +3,23 @@ import os
 import datetime
 from subprocess import call
 
+debug = True
+
 
 # Get all the paths we care about
 cwd = os.getcwd()
 jsdir = os.path.join(cwd, "js")
 cssdir = os.path.join(cwd, "css")
+resdir = os.path.join(cwd, "res")
 output = os.path.join(cwd, "output")
 outjsdir = os.path.join(output, "js")
 outcssdir = os.path.join(output, "css")
+outresdir = os.path.join(output, "res")
 
 nodebin = os.path.join(cwd, "node_modules", ".bin")
 lessbin = os.path.join(nodebin, "lessc.cmd")
-jsminbin = os.path.join(nodebin, "jsmin.cmd")
+handlebarbin = os.path.join(nodebin, "handlebars.cmd")
+uglifybin = os.path.join(nodebin, "uglifyjs.cmd")
 npmbin = "npm.cmd"
 
 # CLean the old stuff.
@@ -27,17 +32,18 @@ print "building output"
 os.mkdir(output)
 os.mkdir(outjsdir)
 os.mkdir(outcssdir)
+os.mkdir(outresdir)
 
 # If we don't have the npm modules installed yet then install them.
-if os.path.exists(lessbin) is False or os.path.exists(jsminbin) is False:
+if os.path.exists(lessbin) is False or os.path.exists(uglifybin) is False or os.path.exists(handlebarbin) is False:
     print "Updating node packages"
     call([npmbin, "update"])
 
 # Copy the cache.manifest file over.
-shutil.copy(os.path.join(cwd, "cache.manifest"), output)
-print "Copying manifest file and appending timestamp"
-with open(os.path.join(output, "cache.manifest"), "a") as manifest:
-    manifest.write("# Manifest Timestamp: " + str(datetime.datetime.now()))
+# shutil.copy(os.path.join(cwd, "cache.manifest"), output)
+# print "Copying manifest file and appending timestamp"
+# with open(os.path.join(output, "cache.manifest"), "a") as manifest:
+#     manifest.write("# Manifest Timestamp: " + str(datetime.datetime.now()))
 
 # Copy the files over.
 files = os.listdir(cwd)
@@ -47,18 +53,28 @@ for file in files:
         print "copying file: ", file
         shutil.copy(filename, output)
 
+# Copy the resources over.
+files = os.listdir(resdir)
+for file in files:
+    filename = os.path.join(resdir, file)
+    if os.path.isfile(filename):
+        print "copying file: ", file
+        shutil.copy(filename, outresdir)
+
+# Copy the javascript files to the output.
 files = os.listdir(jsdir)
 for file in files:
     filename = os.path.join(jsdir, file)
     if os.path.isfile(filename) and filename.endswith(".js"):
-        if filename.endswith(".min.js"):
+        if filename.endswith(".min.js") or debug:
             print "copying file: ", file
             shutil.copy(filename, outjsdir)
         else:
             print "minimizing file: ", file
             outfile = os.path.join(outjsdir, file)
-            call([jsminbin, "-o", outfile, filename])
+            call([uglifybin, filename, "-o", outfile])
 
+# Copy the css files to the output.
 files = os.listdir(cssdir)
 for file in files:
     filename = os.path.join(cssdir, file)
@@ -67,3 +83,10 @@ for file in files:
         outfile = file.replace(".less", ".css")
         outfile = os.path.join(outcssdir, outfile)
         call([lessbin, "-x", filename, outfile])
+
+# Compile the templates.
+print "Compiling templates.js"
+if debug:
+    call([handlebarbin, os.path.join(jsdir, "templates"), "-f", os.path.join(outjsdir, "templates.js")])
+else:
+    call([handlebarbin, "-m", os.path.join(jsdir, "templates"), "-f", os.path.join(outjsdir, "templates.js")])
--- js/main.js
+++ js/main.js
@@ -1,142 +1,143 @@
-/*global _*/
+/*global _, Handlebars.templates*/
 
 (function () {
-    var sampleData = {
-            taskLists: [
-                {
-                    "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDow",
-                    "title": "ajbecker's list",
-                    "updated": "2013-03-07T03:37:44.000Z",
-                    "items": [{
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoyMTAwODc0MzE2",
-                        "title": "Contact USAA to resume applications",
-                        "updated": "2013-02-22T19:31:28.000Z",
-                        "position": "00000000000000001907",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxNzk0NTMyODEx",
-                        "title": "Request Sierra ssn",
-                        "updated": "2013-02-04T18:25:21.000Z",
-                        "position": "00000000000833974220",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxOTY2ODg4MjI4",
-                        "title": "Pay Nelson dentist Bill",
-                        "updated": "2013-02-04T22:35:32.000Z",
-                        "position": "00000000000854823575",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxMjczMzIwMDM0",
-                        "title": "Call American express to reverse charge",
-                        "updated": "2013-02-04T18:25:30.000Z",
-                        "position": "00000000000875672931",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxMjMzNjA3MDk1",
-                        "title": "",
-                        "updated": "2013-02-22T19:31:25.000Z",
-                        "position": "00000000000878279100",
-                        "status": "needsAction"
-                    }
-                ]
-                }, {
-                    "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6NDow",
-                    "title": "Future Tasks",
-                    "updated": "2013-02-04T20:22:38.000Z",
-                    "items": [{
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6NDozNzM0Mjk4MTY",
-                        "title": "2012 taxes",
-                        "updated": "2013-02-04T20:22:38.000Z",
-                        "position": "00000000000000001023",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6NDoyMDA3NzM2NTgx",
-                        "title": "Hang up bicycles",
-                        "updated": "2013-02-04T20:21:50.000Z",
-                        "position": "00000000000000002047",
-                        "status": "needsAction"
-                    }]
-                }, {
-                    "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MjMxMDA0Mzc3OjA",
-                    "title": "Shopping",
-                    "updated": "2013-03-07T01:47:59.000Z",
-                    "items": [{
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MjMxMDA0Mzc3OjEzODQ1MzgwNzg",
-                        "title": "",
-                        "updated": "2013-03-07T01:47:59.000Z",
-                        "position": "00000000000033554431",
-                        "status": "needsAction"
-                    }]
-                }, {
-                    "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjA",
-                    "title": "Work",
-                    "updated": "2013-03-07T03:37:44.000Z",
-                    "items": [{
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
-                        "title": "Fix bleed through issue with CVTY",
-                        "updated": "2013-03-06T22:05:09.000Z",
-                        "position": "00000000003221225471",
-                        "notes": " Submitted bug report\nSource code at http://pastebin.com/460Km1Tk",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3Ojk2NTIxOTA2MA",
-                        "title": "Confirm with ACL that the changes have been made to site.config",
-                        "updated": "2013-02-27T17:22:14.000Z",
-                        "parent": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
-                        "position": "00000000000100663295",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjExNTM4MzE0OTY",
-                        "title": "Integrate bug fix in to source",
-                        "updated": "2013-02-27T16:42:12.000Z",
-                        "parent": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
-                        "position": "00000000000234881023",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3Ojc1Njc1MTY3MQ",
-                        "title": "Deliver bug fix to client",
-                        "updated": "2013-02-27T16:42:11.000Z",
-                        "parent": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
-                        "position": "00000000000268435455",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjExNTIwNjE1NDI",
-                        "title": "Deal with DVT 538492 - webex with Select Health",
-                        "updated": "2013-02-27T18:42:15.000Z",
-                        "position": "00000000003758096383",
-                        "notes": "Search.js - end of file - need to check the Sys.Application._initialized flag\n\nWaiting on response from client\n\nreceived message from client - will get together this afternoon.",
-                        "status": "needsAction",
-                        "due": "2013-02-27T00:00:00.000Z"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjIwMDkwMDU0MzE",
-                        "title": "Outlook integration",
-                        "updated": "2013-03-04T16:21:23.000Z",
-                        "position": "00000000003825205247",
-                        "status": "needsAction"
-                    }, {
-                        "id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE2MDY1OTIwNTE",
-                        "title": "",
-                        "updated": "2013-03-07T03:37:44.000Z",
-                        "position": "00000000003833593855",
-                        "status": "needsAction"
-                    }]
-                }
-            ]
-        },
-        template = _.template([
-            '<ul>',
-            '<%_.each(taskLists, function (list) {%>',
-            '<li><%=list.title%></li>',
-            '<%});%>',
-            '</ul>'
-        ].join('')),
-        populateTaskLists = function () {
-            $("#taskLists").append(template(sampleData));
-        };
+	var sampleData = {
+			taskLists: [
+				{
+					"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDow",
+					"title": "ajbecker's list",
+					"updated": "2013-03-07T03:37:44.000Z",
+					"items": [{
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoyMTAwODc0MzE2",
+						"title": "Contact USAA to resume applications",
+						"updated": "2013-02-22T19:31:28.000Z",
+						"position": "00000000000000001907",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxNzk0NTMyODEx",
+						"title": "Request Sierra ssn",
+						"updated": "2013-02-04T18:25:21.000Z",
+						"position": "00000000000833974220",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxOTY2ODg4MjI4",
+						"title": "Pay Nelson dentist Bill",
+						"updated": "2013-02-04T22:35:32.000Z",
+						"position": "00000000000854823575",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxMjczMzIwMDM0",
+						"title": "Call American express to reverse charge",
+						"updated": "2013-02-04T18:25:30.000Z",
+						"position": "00000000000875672931",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MDoxMjMzNjA3MDk1",
+						"title": "",
+						"updated": "2013-02-22T19:31:25.000Z",
+						"position": "00000000000878279100",
+						"status": "needsAction"
+					}
+				]
+				}, {
+					"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6NDow",
+					"title": "Future Tasks",
+					"updated": "2013-02-04T20:22:38.000Z",
+					"items": [{
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6NDozNzM0Mjk4MTY",
+						"title": "2012 taxes",
+						"updated": "2013-02-04T20:22:38.000Z",
+						"position": "00000000000000001023",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6NDoyMDA3NzM2NTgx",
+						"title": "Hang up bicycles",
+						"updated": "2013-02-04T20:21:50.000Z",
+						"position": "00000000000000002047",
+						"status": "needsAction"
+					}]
+				}, {
+					"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MjMxMDA0Mzc3OjA",
+					"title": "Shopping",
+					"updated": "2013-03-07T01:47:59.000Z",
+					"items": [{
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6MjMxMDA0Mzc3OjEzODQ1MzgwNzg",
+						"title": "",
+						"updated": "2013-03-07T01:47:59.000Z",
+						"position": "00000000000033554431",
+						"status": "needsAction"
+					}]
+				}, {
+					"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjA",
+					"title": "Work",
+					"updated": "2013-03-07T03:37:44.000Z",
+					"items": [{
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
+						"title": "Fix bleed through issue with CVTY",
+						"updated": "2013-03-06T22:05:09.000Z",
+						"position": "00000000003221225471",
+						"notes": " Submitted bug report\nSource code at http://pastebin.com/460Km1Tk",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3Ojk2NTIxOTA2MA",
+						"title": "Confirm with ACL that the changes have been made to site.config",
+						"updated": "2013-02-27T17:22:14.000Z",
+						"parent": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
+						"position": "00000000000100663295",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjExNTM4MzE0OTY",
+						"title": "Integrate bug fix in to source",
+						"updated": "2013-02-27T16:42:12.000Z",
+						"parent": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
+						"position": "00000000000234881023",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3Ojc1Njc1MTY3MQ",
+						"title": "Deliver bug fix to client",
+						"updated": "2013-02-27T16:42:11.000Z",
+						"parent": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE3ODQ0MTIzODI",
+						"position": "00000000000268435455",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjExNTIwNjE1NDI",
+						"title": "Deal with DVT 538492 - webex with Select Health",
+						"updated": "2013-02-27T18:42:15.000Z",
+						"position": "00000000003758096383",
+						"notes": "Search.js - end of file - need to check the Sys.Application._initialized flag\n\nWaiting on response from client\n\nreceived message from client - will get together this afternoon.",
+						"status": "needsAction",
+						"due": "2013-02-27T00:00:00.000Z"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjIwMDkwMDU0MzE",
+						"title": "Outlook integration",
+						"updated": "2013-03-04T16:21:23.000Z",
+						"position": "00000000003825205247",
+						"status": "needsAction"
+					}, {
+						"id": "MDQ5OTUxODEyMjkyNTg4MDc0Mzg6OTA1OTU1MTM3OjE2MDY1OTIwNTE",
+						"title": "",
+						"updated": "2013-03-07T03:37:44.000Z",
+						"position": "00000000003833593855",
+						"status": "needsAction"
+					}]
+				}
+			]
+		},
+		populateTaskLists = function () {
+			var data = {
+				listClass: "taskList",
+				items: _(sampleData.taskLists).map(function (item) {
+					return {
+						itemClass: "taskList",
+						item: item.title
+					};
+				})
+			};
+			$("#taskLists").append(Handlebars.templates.simplelist(data));
+		};
 
-    $(function () {
+	$(function () {
 
-        populateTaskLists();
-    });
-   	
+		populateTaskLists();
+	});
 }());
